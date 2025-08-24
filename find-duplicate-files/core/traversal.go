b841d7e28f0c89a05c9efb701144c29602c6bf47
Vivek Ajbe <vajbe.tce@mail.com>
2025-08-24 15:19:59 +0000

    adding logs

--- find-duplicate-files/core/traversal.go
+++ find-duplicate-files/core/traversal.go
@@ -18,14 +18,15 @@ func TraverseDirectory(dir string) {
 	var m runtime.MemStats
 	globalCache = make(map[string][]string)
 	fileSystem := os.DirFS(dir)
-
+	startTime := time.Now()
+	fmt.Println("Starting process: ", startTime)
 	fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		if !d.IsDir() {
-			fmt.Println("Started reading: ", d.Name(), " at ", time.Now(), "\tNumGC = ", m.NumGC)
+			fmt.Println("Started reading: \t", d.Name(), " \tat ", time.Now(), "\tNumGC = ", m.NumGC)
 			absolutePath := dir + "/" + path
 
 			f, err := os.Open(absolutePath)
@@ -48,7 +49,7 @@ func TraverseDirectory(dir string) {
 			}
 
 			globalCache[hash] = append(globalCache[hash], absolutePath)
-			fmt.Println("Finished reading: ", d.Name(), " at ", time.Now(), "\tNumGC = ", m.NumGC)
+			fmt.Println("Finished reading: \t", d.Name(), " \tat ", time.Now(), "\tNumGC = ", m.NumGC)
 		}
 		return nil
 	})
@@ -58,5 +59,7 @@ func TraverseDirectory(dir string) {
 			fmt.Print(value)
 		}
 	}
+
+	fmt.Println("Ending at: ", time.Since(startTime))
 
 }
