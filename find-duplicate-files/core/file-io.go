ba8740aa6a666922fc5e0da54f3caebccf76e666
Vivek Ajbe <vajbe.tce@mail.com>
2025-08-28 04:17:01 +0000

    adding a basic skeleton for streamed data read

--- find-duplicate-files/core/file-io.go
+++ find-duplicate-files/core/file-io.go
@@ -5,6 +5,14 @@ import (
 	"os"
 )
 
+/* type FileChunk struct {
+	path      string
+	offset    int
+	chunkSize int
+} */
+
+var DEFAULT_CHUNK_SIZE int = 5
+
 func ReadFile(filePath string) {
 
 	f, err := os.Open(filePath)
@@ -15,10 +23,25 @@ func ReadFile(filePath string) {
 	}
 
 	defer f.Close()
-	b := make([]byte, 5000)
-	f.Read(b)
 
-	fmt.Println("File contents: ", string(b))
+	stat, err := os.Stat(filePath)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	fileSize := stat.Size()
+
+	numWorkers := fileSize / int64(DEFAULT_CHUNK_SIZE)
+
+	fmt.Println("Workers: ", numWorkers, " File Size: ", fileSize)
+
+	for i := 1; i <= int(numWorkers); i++ {
+		nextChunk := i * DEFAULT_CHUNK_SIZE
+		buffer := make([]byte, DEFAULT_CHUNK_SIZE)
+		f.ReadAt(buffer, int64(DEFAULT_CHUNK_SIZE))
+		fmt.Println(string(buffer))
+		fmt.Printf("\nFile: %s Chunk Size: %d Processed At: %d\n", filePath, DEFAULT_CHUNK_SIZE, nextChunk)
+	}
 	fmt.Println()
-
 }
